2687df1757ab0611e11d94a5191798107a871b0a
unknown <imbilalj@gmail.com>
2019-03-19 21:16:52 +0500

    Added new files and folders

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="author" content="Bilal Javed">
+    <title>Complete CMS</title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="logo">
+          <h1><span class="highlight">Complete</span> CMS</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="current"><a href="index.html">Home</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li class="btn-nav"><a href="login.php">Login</a></li>
+            <li class="btn-nav"><a href="register.php">Register</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section id="showcase">
+      <div class="container">
+        <h1>Our Content Management System</h1>
+        <p>One Line Introduction Of Our Project</p>
+      </div>
+    </section>
+
+    <section id="newsletter">
+      <div class="container">
+        <h1>Subscribe to Newsletter</h1>
+        <form>
+          <input type="email" placeholder="Enter your Email">
+          <button type="submit" class="btn">Subscribe</button>
+        </form>
+      </div>
+    </section>
+
+    <section id="boxes">
+      <div class="container">
+        <div class="box">
+          <img src="./img/front.png">
+          <h3>Service One</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta massa id erat venenatis</p>
+        </div>
+        <div class="box">
+          <img src="./img/back.png">
+          <h3>Service Two</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta massa id erat venenatis</p>
+        </div>
+        <div class="box">
+          <img src="./img/wordpress.png">
+          <h3>Service Three</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta massa id erat venenatis</p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>Complete CMS Copyright &copy; 2019</p>
+    </footer>
+
+  </body>
+</html>
