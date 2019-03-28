7ae33d8df3bb3a6ca3d82e6eed145b871de7bd20
Hams Ansari <hamsansari17@gmail.com>
2019-03-28 17:17:40 +0500

    Copyright Changes

--- backend/logic/admin/pages.php
+++ backend/logic/admin/pages.php
@@ -105,7 +105,7 @@
               <h4>Disk Space Used</h4>
               <div class="progress">
                   <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                      60%
+                      50%
               </div>
             </div>
             <h4>Bandwidth Used </h4>
